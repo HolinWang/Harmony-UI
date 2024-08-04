49a28f6db2c0fbed4594cdc7d610967e4f627338
Holin <Holin.wang@outlook.com>
2024-08-04 18:16:23 +0800

    在组件文档中查看完整的示例代码

--- src/components/Button/stories/index.stories.ts
+++ src/components/Button/stories/index.stories.ts
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import { Button } from 'harmony-ui';
+// @ts-expect-error - required for ts
+import CustomSource from './Custom.stories?raw';
+import { Custom } from './Custom.stories';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -50,5 +53,13 @@ export const Small: Story = {
     label: 'Button',
   },
 };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(Custom as any).parameters = {
+  docs: {
+    source: {
+      code: CustomSource,
+    },
+  },
+};
 
-export {Custom} from './Custom.stories'
+export {Custom};

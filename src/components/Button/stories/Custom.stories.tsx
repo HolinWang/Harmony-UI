96dd65c70afdf8ea9ab8cc57b1ccbd9dcc2bd894
Holin <Holin.wang@outlook.com>
2024-08-04 17:57:41 +0800

    新增css解决方案库

--- src/components/Button/stories/Custom.stories.tsx
+++ src/components/Button/stories/Custom.stories.tsx
@@ -1,6 +1,18 @@
-export const Custom = {
-  args: {
-    primary: true,
-    label: 'Custom',
+import { makeStyles } from "@griffel/react";
+import { Button } from "harmony-ui";
+
+const useClasses = makeStyles({
+  root: {
+    backgroundColor: "#fff",
+    padding: "100px",
   },
+});
+
+export const Custom = () => {
+  const styles = useClasses();
+  return (
+    <div className={styles.root}>
+      <Button primary label="Button" />
+    </div>
+  );
 };
